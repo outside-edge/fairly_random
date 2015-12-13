b3f1c9573d188b31a035168557800c6df3310eec
Gaurav Sood <gsood07@gmail.com>
2015-12-13 12:29:39 -0500

    crick update

--- scripts/05_cricket.R
+++ scripts/05_cricket.R
@@ -14,6 +14,7 @@ source("scripts/04_merge_ranking_grounds_data.R")
 library(plyr)
 library(stringi)
 library(xtable)
+library(tidyr)
 
 # Read in data/ now sourcing it - see 04_merge_ranking_data
 # cricket <- read.csv("data/final_output.csv")
@@ -80,9 +81,14 @@ cricket$home_toss_win_t <- stri_detect_fixed(cricket$ground, cricket$win_toss, c
                            stri_detect_fixed(cricket$team2, cricket$country, case_insensitive=TRUE) |
                            stri_detect_fixed(cricket$country, cricket$team1, case_insensitive=TRUE) | 
                            stri_detect_fixed(cricket$country, cricket$team2, case_insensitive=TRUE)
-cricket$home_country_data  <- cricket$country == cricket$team1 | cricket$country==cricket$team2
 
 "
+
+cricket$home_country_data  <- cricket$country == cricket$team1 | cricket$country==cricket$team2
+
+# Home country wins toss
+cricket$home_wins_toss  <- ifelse(cricket$country==cricket$team1, cricket$team1_win_toss, cricket$team2_win_toss)
+# with(cricket[cricket$home_country_data==1,], mean(home_wins_toss))
 
 
 "
@@ -90,28 +96,32 @@ Melt the data
 Two rows per match
 "
 
-# "team1", "team2", "team1_id", "team2_id", "team1_rank", "team2_rank", "team1_win_toss", "team2_win_toss", "team1_win_game", "team2_win_game", 
-# "tossgame", "win_toss", "bat_or_bowl", "outcome", "win_game"
-
 # Match level vars: 
 match_cols <- c("url", "date", "day_n_night", "ground", "rain", "duckworth_lewis", "match_id", "type_of_match", "month", "year", "diff_ranks", 
-      "signed_diff_ranks", "ground_id", "country", "continent", "latitude", "longitude", "uniqueid", "draw")
+      "signed_diff_ranks", "ground_id", "country", "continent", "latitude", "longitude", "uniqueid", "draw", "outcome")
 
+# Team cols, rename for gather/separate to work well
 team_cols <- c("team1", "team2", "team1_id", "team2_id", "team2_rank", "team1_rank", "team1_win_toss", "team2_win_toss", "team1_win_game", "team2_win_game")
 rename_cols <- c("team1.name", "team2.name", "team1.id", "team2.id", "team2.rank", "team1.rank", "team1.wintoss", "team2.wintoss", "team1.wingame", "team2.wingame")
 names(cricket)[names(cricket) %in% team_cols] <- rename_cols
 
+# Melt
 crickett <- cricket %>% gather(key, value, starts_with('team')) %>% separate(key, c("var", "col")) %>% arrange(url) %>% spread(col, value)
 
+"
+Recode, Fix Variable Type
+"
 # The game is drawn
 crickett$wingame[crickett$draw==1] <- .5
 
 crickett$bat_bowl     <- ifelse(crickett$wintoss, crickett$bat_or_bowl, ifelse(crickett$bat_or_bowl=="bowl", "bat", "bowl"))
 crickett$home_country <- crickett$country == crickett$name
-# crickett$countries    <- crickett$country == crickett$team1 | crickett$country == crickett$team2
 
 crickett$wintoss <- as.numeric(crickett$wintoss)
 crickett$wingame <- as.numeric(crickett$wingame)
+
+# Data Integrity
+# ddply(crickett, ~type_of_match + day_n_night, summarise, mean=mean(wintoss))
 
 "
 Analysis
@@ -119,11 +129,12 @@ Analysis
 1. Do teams win more tosses at home?
    Evidence from International Matches
 "
+# Proportion of tosses won in home country
+# Proportion of tosses won when playing away from home
 
-homet <- with(crickett, xtabs( ~ home_country + wintoss))
-
-ddply(crickett, ~type_of_match + day_n_night, summarise, mean=mean(wintoss))
-
+homet <- with(crickett[(crickett$home_country_data==1),], xtabs( ~ home_country + wintoss))
+homet/rowSums(homet)
+binom.test(2860, 5591, p=.5)
 
 "
 Note: 
@@ -137,19 +148,20 @@ res  <- wint/colSums(wint)
 res
 res[3,2] - res[3,1]
 
-cricket$home_toss_win <- cricket$country == cricket$win_toss
-sum(cricket$home_toss_win)/sum(cricket$country_data)
-binom.test(2433, 4749, p=.5)
-
 # By Type of Match
 ddply(crickett, ~type_of_match + wintoss, summarise, mean = mean(wingame==1), n = length(unique(uniqueid)),  se=2*100*sqrt(mean*(1-mean)/n))
-ddply(crickett, ~day_n_night +   wintoss, summarise,   mean = mean(wingame==1), n = length(unique(uniqueid)), se=2*100*sqrt(mean*(1-mean)/n))
-ddply(crickett, ~duckworth_lewis + toss_win, summarise, mean = mean(game_win==1), n = length(unique(uniqueid)), se=2*100*sqrt(mean*(1-mean)/n))
-ddply(crickett, ~type_of_match + day_n_night + toss_win, summarise, mean = mean(game_win==1), n = length(unique(uniqueid)),  se=2*100*sqrt(mean*(1-mean)/n))
-ddply(crickett, ~type_of_match + duckworth_lewis + toss_win, summarise, mean = mean(game_win==1), n = length(unique(uniqueid)),  se=2*100*sqrt(mean*(1-mean)/n))
 
-with(crickett, glm())
-# Figs
+# By Day/N/Night
+ddply(crickett, ~day_n_night +   wintoss, summarise,   mean = mean(wingame==1), n = length(unique(uniqueid)), se=2*100*sqrt(mean*(1-mean)/n))
+
+# By Duckworth Lewis
+ddply(crickett, ~duckworth_lewis + toss_win, summarise, mean = mean(wingame==1), n = length(unique(uniqueid)), se=2*100*sqrt(mean*(1-mean)/n))
+
+# By Type of Match, Day/N Night
+ddply(crickett, ~type_of_match + day_n_night, summarise, mean = mean(wingame==1), n = length(unique(uniqueid)),  se=2*100*sqrt(mean*(1-mean)/n))
+
+# By Type of Match, D/L
+ddply(crickett, ~type_of_match + duckworth_lewis, summarise, mean = mean(wingame==1), n = length(unique(uniqueid)),  se=2*100*sqrt(mean*(1-mean)/n))
 
 # Fig libs
 library(ggplot2)
@@ -163,26 +175,33 @@ cricket$type_of_match <- factor(cricket$type_of_match, levels=c("FC", "TEST", "L
 Win By Match Type
 "
 
-win_match_type <- ddply(crickett, ~type_of_match + toss_win, summarise, perc = mean(game_win)*100, n = length(unique(uniqueid)))
+win_match_type <- ddply(crickett, ~type_of_match, summarise, diff = mean(wingame[wintoss==1]) - mean(wingame[wintoss==0]))
+win_match_type$diff <- win_match_type$diff*100
 
-ggplot(win_match_type, aes(type_of_match, diff)) + 
-geom_bar(stat = "identity", position = "identity", fill="#42C4C7") + 
+ggplot(win_match_type, aes(x=type_of_match, y=diff)) + 
+geom_bar(stat = "identity", fill="#42c4c7") + 
 theme_minimal() + 
 xlab("") +
-scale_y_continuous(breaks=seq(-1, 7, 1), labels=nolead0s(seq(-1, 7, 1)), limits=c(-1, 7), name="") +
+scale_y_continuous(breaks=seq(0, 10, 1), labels= paste0(nolead0s(seq(0, 10, 1)), "%"), limits=c(0, 10), name="") +
 theme(panel.grid.major.y = element_line(colour = "#e3e3e3", linetype = "dotted"),
-	  panel.grid.minor.x = element_blank(),
-	  panel.grid.major.x = element_line(colour = "#f3f3f3", linetype = "solid"),
-	  panel.border       = element_blank(),
-	  legend.position  = "none",
- 	  title        = element_text(size=8),
-	  axis.title   = element_text(size=8),
-	  axis.text    = element_text(size=8),
-	  axis.ticks.y = element_blank(),
-	  axis.ticks.x = element_line(colour = '#f1f1f1'),
-	  strip.text.x =  element_text(size=9),
-	  legend.text=element_text(size=8))
-ggsave("figs/winbyType.pdf")
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_line(colour = "#f7f7f7", linetype = "solid"),
+      panel.border       = element_blank(),
+      legend.position    = "bottom",
+      legend.text        = element_text(size=10),
+      legend.background  = element_rect(color="#ffffff"),
+      legend.key         = element_rect(color="#ffffff", fill="#ffffff"),
+      legend.key.size    = unit(.1,"cm"),
+      legend.margin      = unit(.2,"cm"),
+      title              = element_text(size=8),
+      axis.title         = element_text(size=8),
+      axis.text          = element_text(size=8),
+      axis.ticks.y       = element_blank(),
+      axis.ticks.x       = element_line(colour = '#f1f1f1'),
+      strip.text.x       = element_text(size=9),
+      legend.text        = element_text(size=8),
+      plot.margin        = unit(c(0,.5,.5,.5), "cm"))
+ggsave("figs/winbyType.pdf", width=7)
 
 "
 Win by Day/Night
