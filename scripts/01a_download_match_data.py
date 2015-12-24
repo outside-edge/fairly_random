fcc02e5b570a514e53a40632d0a6c2e6a3c0d872
Derek Willis <dwillis@gmail.com>
2015-12-24 10:36:30 -0500

    updated scripts

--- scripts/01a_download_match_data.py
+++ scripts/01a_download_match_data.py
@@ -19,14 +19,14 @@ import simplejson
 import unicodedata
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 
-for match_type in ['odi', 'test', 't20i', 't20', 'list%20a', 'first%20class']:
+for match_type in ['list%20a', 'first%20class']: # 'odi', 'test', 't20i', 't20', 
     results = []
     r = requests.get('http://search.espncricinfo.com/ci/content/match/search.html?all=1;page=0;search=' + match_type)
     soup = BeautifulSoup(r.text)
     last_match = int(soup.findAll('span', attrs={'class':'PaginationNmbrs'})[-1].text)
     last_page = int(math.ceil(float(last_match)/float(20)))
     for i in range(0, last_page):
-        time.sleep(2)
+        time.sleep(1)
         results_page = requests.get("http://search.espncricinfo.com/ci/content/match/search.html?search={0};all=1;page={1}".format(match_type, i))
         soupy = BeautifulSoup(results_page.text)
         for new_host in soupy.findAll('a', {'class' : 'srchPlyrNmTxt'}):
--- scripts/02_parse_cric.py
+++ scripts/02_parse_cric.py
@@ -10,10 +10,11 @@ from espncricinfo.match import Match
 
 headers = ["team1", "team1_id", "team2", "team2_id", "win_toss", "bat_or_bowl", "outcome", "win_game", "date", "day_n_night", "ground", "rain", "duckworth_lewis", "match_id", "type_of_match", "home_team_id", "umpire_1_id", "umpire_1_name", "umpire_1_country", "umpire_2_id", "umpire_2_name", "umpire_2_country", "tv_umpire_id", "tv_umpire_name", "tv_umpire_country", "referree_id", "referee_name", "referee_country"]
 
-results = simplejson.loads(open('matches-t20.json').read())
+results = simplejson.loads(open('matches-first-class.json').read())
+results = list(set(results))[9193:-1]
 
 ##################################START PROCESSING DATA#########################################
-with open("t20_output.csv", "wb") as csvfile:
+with open("first_class_output2.csv", "wb") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(headers)
     for result in results:
@@ -22,13 +23,39 @@ with open("t20_output.csv", "wb") as csvfile:
             m = Match(int(result))
             if m.match_json()['match_status'] == 'forthcoming':
                 continue
+            if m.result() == '':
+                continue
             if m.rain_rule() == 'D/L method':
                 duckworth_lewis = 1
             else:
                 duckworth_lewis = 0
-            [ump_1, ump_2] = [o for o in m.officials() if o['player_type_name'] == 'umpire']
-            tv_ump = [o for o in m.officials() if o['player_type_name'] == 'tv umpire']
-            match_ref = [o for o in m.officials() if o['player_type_name'] == 'referee']
+            try:
+                m.team_2()['team_name']
+            except KeyError:
+                continue
+            try:
+                m.team_1()['team_name']
+            except KeyError:
+                continue
+            try:
+                if len(m.officials()) > 0:
+                    umpires = [o for o in m.officials() if o['player_type_name'] == 'umpire']
+                    tv_ump = [o for o in m.officials() if o['player_type_name'] == 'tv umpire']
+                    match_ref = [o for o in m.officials() if o['player_type_name'] == 'referee']
+                    if len(umpires) == 2:
+                        ump_1 = umpires[0]
+                        ump_2 = umpires[1]
+                    elif len(umpires) == 0:
+                        ump_1 = {'object_id': None, 'known_as': None, 'team_abbreviation': None}
+                        ump_2 = {'object_id': None, 'known_as': None, 'team_abbreviation': None}
+                    else:
+                        ump_1 = umpires[0]
+                        ump_2 = {'object_id': None, 'known_as': None, 'team_abbreviation': None}
+                else:
+                    ump_1 = {'object_id': None, 'known_as': None, 'team_abbreviation': None}
+                    ump_2 = {'object_id': None, 'known_as': None, 'team_abbreviation': None}
+            except ValueError:
+                raise
             if len(tv_ump) > 0:
                 tvu_id = tv_ump[0]['object_id']
                 tvu_name = tv_ump[0]['known_as']
@@ -46,8 +73,7 @@ with open("t20_output.csv", "wb") as csvfile:
                 mr_name = None
                 mr_country = None
             writer.writerow([m.team_1()['team_name'], m.team_1_id(), m.team_2()['team_name'], m.team_2_id(), m.toss_winner(), m.toss_decision(), m.result(), m.match_winner(), m.date(), m.lighting(), m.ground_name(), None, duckworth_lewis, result, m.match_class(), m.match_json()['home_team_id'], ump_1['object_id'], ump_1['known_as'], ump_1['team_abbreviation'], ump_2['object_id'], ump_2['known_as'], ump_2['team_abbreviation'], tvu_id, tvu_name, tvu_country, mr_id, mr_name, mr_country])
-        except:
-            print int(result)
+        except simplejson.scanner.JSONDecodeError:
             continue
 
 ##################################FINISHED#########################################
